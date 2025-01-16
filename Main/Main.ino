8d07f312519ad071954e02c6cf93b9df919c48ab
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-01-16 08:21:01 +0800

    Add notes for calibration

--- Main/Main.ino
+++ Main/Main.ino
@@ -12,9 +12,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  readSensor();
-  calibrateMQ2();
-  calibrateMQ135();
+  readSensor(); //disregards this function once you will get the actuail baseline of the 2 sensors
+  // Calibrate the MQ2 sensor to get the baseline reading for smoke detection.
+  // Note: Test this in the actual environment where the device will be implemented 
+  // to get an accurate baseline reading.
+  float baseline = calibrateMQ2(); 
+  Serial.print("MQ2 Baseline Result: ");
+  Serial.println(baseline); // Print the baseline of MQ2 Smoke reading
+  float baseline1 = calibrateMQ135();
+  Serial.print("MQ135 Baseline Resut: ");
+  Serial.println(baseline1); // Print the baseline of MQ135 Smoke reading
   delay(1000);
 }
 
