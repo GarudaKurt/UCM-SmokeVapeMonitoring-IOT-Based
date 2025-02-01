d9be28fbad5b1f52198f9001a232163a3959aada
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-01 09:06:10 +0800

    Update Sensors.h

--- Main/Sensors.h
+++ Main/Sensors.h
@@ -2,8 +2,8 @@
 #define __SENSORS__H
 
 void initSensors();
-float readMQ2Value();
-float readMQ135Value();
+int32_t readMQ2Value();
+int32_t readMQ135Value();
 float calibrateMQ135();
 float calibrateMQ2();
 
