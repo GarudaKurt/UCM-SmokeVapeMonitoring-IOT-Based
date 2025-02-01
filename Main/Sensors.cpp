c3d8ab3a89f24d9066cbc7f467aa427adb50b3fd
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-01 09:05:47 +0800

    Update Sensors.cpp

--- Main/Sensors.cpp
+++ Main/Sensors.cpp
@@ -3,6 +3,7 @@
 #include <MQ2.h>
 #include <MQ135.h>
 #include <DHT11.h>
+#include <Wire.h>
 
 const int PIN_MQ2 = 33;
 const int PIN_MQ135 = 32;
@@ -13,18 +14,23 @@ DHT11 dht11(DHT_PIN);
 MQ135 mq135_sensor(PIN_MQ135);
 MQ2 mq2(PIN_MQ2);
 
-float smokeMQ2ppm, smokeMQ135ppm = 0;
+int32_t smokeMQ2ppm = 0;
+int32_t smokeMQ135ppm = 0;
 int temperature = 0;
 int humidity = 0;
 
 void initSensors() {
+  Wire.begin(23, 22);
+  pinMode(PIN_MQ135, INPUT);
+  pinMode(PIN_MQ132, INPUT);
   analogReadResolution(12);
   analogSetAttenuation(ADC_11db); // 0-3.3V range
 }
 
-float readMQ2Value() {
+int32_t readMQ2Value() {
   smokeMQ2ppm = analogRead(PIN_MQ2);
-  return smokeMQ2ppm / 4.095;
+  return smokeMQ2ppm;
+  //return smokeMQ2ppm / 4.095;
 }
 float calibrateMQ2() {
   return mq2.readSmoke();
@@ -34,9 +40,10 @@ float calibrateMQ135() {
   return mq135_sensor.getCorrectedPPM(temperature, humidity);;
 }
 
-float readMQ135Value() {
+int32_t readMQ135Value() {
   smokeMQ135ppm = analogRead(PIN_MQ135);
-  return smokeMQ135ppm / 4.095;
+  return smokeMQ135ppm;
+  //return smokeMQ135ppm / 4.095;
 }
 
 
