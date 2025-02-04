092672d1d987e7e9eb9e7f54c5c6a08a0d00d8cb
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-04 13:09:41 +0800

    Update monitoring.ino

--- monitoring/monitoring.ino
+++ monitoring/monitoring.ino
@@ -5,12 +5,8 @@
 #include "configFirebase.h"
 #include "Display.h"
 
-const int minThreshold = 100;
 const int maxThreshold = 500;
 const int buzzerPin = 11;
-
-int mq2Val = 0;
-int mq135Val = 0;
 
 unsigned long prevMillis = 0;
 const int interval = 1000;
@@ -31,19 +27,19 @@ void loop() {
   if (currentMillis - prevMillis >= interval) {
     prevMillis = currentMillis;
 
-    mq2Val = readMQ2Sensor();
-    mq135Val = readMQ135Sensor();
+    int mq2Val = readMQ2Sensor();
+    int smokePPM = readSmoke();
 
-    if(mq2Val >= minThreshold && mq135Val <= maxThreshold) {
+    if(smokePPM >= maxThreshold) {
       Serial.print("MQ2 Value: ");
       Serial.println(mq2Val);
-      Serial.print("MQ135 Value: ");
-      Serial.println(mq135Val);
+      Serial.print("Smoke PPM: ");
+      Serial.println(smokePPM);
       alarmDisplay();
       buzzerStart();
       isAlarmActivate = true;
     } else {
-      updateDisplay(mq2Val, mq135Val, lastTime, lastDate);
+      updateDisplay(smokePPM, lastTime, lastDate);
       Serial.print("MQ2 Value: ");
       Serial.println(mq2Val);
       Serial.print("MQ135 Value: ");
@@ -51,7 +47,7 @@ void loop() {
       if(isAlarmActivate){
         lastTime =  getTime().c_str();
         lastDate =  getDate().c_str()
-        updateDisplay(mq2Val, mq135Val, getTime().c_str(), getDate().c_str());
+        updateDisplay(smokePPM, getTime().c_str(), getDate().c_str());
         isAlarmActivate = false;
      }
       buzzerStop();
