bc39c100d3e62a0310b404de71173687a4bb2aa0
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-10 20:52:53 +0800

    Update Display.h

--- monitoring/Display.h
+++ monitoring/Display.h
@@ -4,7 +4,7 @@
 void initLCD();
 void wifiDisplayError();
 void alarmDisplay();
-void updateDisplay(int mq135ppm, int mq2ppm, const char *time, const char *date)
+void updateDisplay(int mq135ppm, int mq2ppm, const char *time, const char *date);
 void clearDisplay();
 
 #endif
