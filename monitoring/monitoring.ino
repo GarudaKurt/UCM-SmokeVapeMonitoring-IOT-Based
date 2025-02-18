961a39c537d7e481758e074bdb1381f71cdb88bc
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-18 22:32:15 +0800

    Update monitoring.ino

--- monitoring/monitoring.ino
+++ monitoring/monitoring.ino
@@ -16,17 +16,24 @@ int smokePPM1 = 0;
 
 const int sampleSize = 20;
 const int averagingSize = 15; 
-int calculatedThreshold = 0;
+int calculatedThreshold_MQ2 = 0;
+int calculatedThreshold_MQ135 = 0;
 int smokeSamples[sampleSize];
-int sampleIndex = 0;
+int smokeSamples1[sampleSize];
 int sampleCount = 0;
-bool samplesCollected = false;
 
-// We need to calculate the average smoke PPM from the collected 15 samples then get the average
-int calculateAverageSmoke() {
+int calculateAverageSmoke_MQ2() {
     int sum = 0;
     for (int i = 0; i < averagingSize; i++) {
         sum += smokeSamples[i];
+    }
+    return sum / averagingSize;
+}
+
+int calculateAverageSmoke_MQ135() {
+    int sum = 0;
+    for (int i = 0; i < averagingSize; i++) {
+        sum += smokeSamples1[i];
     }
     return sum / averagingSize;
 }
@@ -42,39 +49,47 @@ void loop() {
   unsigned long currentMillis = millis();
 
   if (currentMillis - prevMillis >= interval) {
-
     smokePPM = readMQ2Sensor();
     smokePPM1 = readSmoke();
 
-    // Collect all samples in the array
     if (sampleCount < sampleSize) {
         smokeSamples[sampleCount] = smokePPM;
+        smokeSamples1[sampleCount] = smokePPM1;
         sampleCount++;
     }
 
     if (sampleCount >= sampleSize) {
         for (int i = 0; i < averagingSize - 1; i++) {
             smokeSamples[i] = smokeSamples[i + 1];
+            smokeSamples1[i] = smokeSamples1[i + 1];
         }
         smokeSamples[averagingSize - 1] = smokePPM;
+        smokeSamples1[averagingSize - 1] = smokePPM1;
 
-        int avgSmoke = calculateAverageSmoke();
-        calculatedThreshold = avgSmoke + 100;  //We need to add on +100 for smoke are occuring in the area
+        int avgMQ2 = calculateAverageSmoke_MQ2();
+        int avgMQ135 = calculateAverageSmoke_MQ135();
+        calculatedThreshold_MQ2 = avgMQ2+ 100;
+        calculatedThreshold_MQ135 = avgMQ135+ 100;
 
-        Serial.print("Calculated Average Smoke: ");
-        Serial.println(avgSmoke);
-        Serial.print("Adaptive Threshold: ");
+        Serial.print("Average Smoke MQ2: ");
+        Serial.println(avgMQ2);
+        Serial.print("Average Smoke MQ135: ");
+        Serial.println(avgMQ135);
+        Serial.print("Calculated Threshold: ");
         Serial.println(calculatedThreshold);
     }
 
-    // Check if the threshold is exceeded
     Serial.print("Threshold: ");
     Serial.println(calculatedThreshold);
 
-    if (calculatedThreshold != 0 && smokePPM >= calculatedThreshold) {
-        Serial.print("MQ2 Value: ");
+    if (calculatedThreshold_MQ2 != 0 && smokePPM >= calculatedThreshold_MQ2) {
+        Serial.print("MQ2 Value PPM: ");
         Serial.println(smokePPM);
-        Serial.print("Smoke PPM: ");
+        alarmDisplay();
+        buzzerStart();
+        isAlarmActivate = true;
+    } else if (calculatedThreshold_MQ135 != 0 && smokePPM1 >= calculatedThreshold_MQ135) {
+        Serial.print("MQ135 Value PPM: ");
         Serial.println(smokePPM1);
         alarmDisplay();
         buzzerStart();
