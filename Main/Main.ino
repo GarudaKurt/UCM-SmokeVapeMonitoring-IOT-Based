2bc8f19fed5fde575d21b4b31e446a9731c6ef51
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-18 22:20:12 +0800

    Update Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -12,6 +12,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("MQ2 Value: ");
+  Serial.println(readMQ2Value());
+  Serial.print("MQ135 Value: ");
+  Serial.println(readMQ135Value());
   readSensor();
   delay(1000);
 }
